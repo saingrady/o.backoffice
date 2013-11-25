2e90675137678017f75c34ca07c8820696ad2e47
Rady Saing <saingrady@gmail.com>
2013-11-25 22:01:23 +0700

    converter remove month array

--- static/js/util/converter.js
+++ static/js/util/converter.js
@@ -26,22 +26,6 @@ Converter.prototype.month = ["January",
  * */
 Converter.prototype.convertLong = function (milis){
 	
-	var month=new Array();
-
-	month[0]="January";
-	month[1]="February";
-	month[2]="March";
-	month[3]="April";
-	month[4]="May";
-	month[5]="June";
-	month[6]="July";
-	month[7]="August";
-	month[8]="September";
-	month[9]="October";
-	month[10]="November";
-	month[11]="December";
-
-
 	var date = new Date(milis);
 	//return $.datepicker.formatDate('DD/MM/YYYY', date);
 	return date.getDate() + "/" + Converter.prototype.month[date.getMonth()] + "/" + date.getFullYear() + " " + 
