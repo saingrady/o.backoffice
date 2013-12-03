93fd22348f35ca0f7c239461f9c09226996e9f7c
Rady Saing <rds@goldengekko.com>
2013-12-03 14:53:00 +0700

    common should be good for jQuery.ajaxSetup - index or signup or home

--- static/js/common/common.js
+++ static/js/common/common.js
@@ -11,3 +11,11 @@ Common.prototype.test = function (){
 	return "hello test";
 };
 
+
+/*
+jQuery.ajaxSetup({
+    beforeSend: function (jqXHR, settings) {
+        jqXHR.setRequestHeader('Authorization', ("Basic " + window.btoa("o:o")));
+    }
+});
+*/
